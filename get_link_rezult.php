7e28ab736b6c6adc94df5f26d910acbd6314c3c3
Boris O <boris.ovodov@gmail.com>
2019-10-23 11:52:32 +0300

    Исправил несколько ошибок для того что бы код работал.

--- get_link_rezult.php
+++ get_link_rezult.php
@@ -5,13 +5,11 @@
       mysql_select_db($dbname);
 
       $id_user = $_GET['option4'];
-
-      function zada4a4($id_user) {
        $query ="SELECT id_steps  FROM `list_status_process` WHERE id_client = $id_user and id_result IS NULL";
        $result = mysql_query($query);
        $num_rows = mysql_num_rows($result);
        if ($num_rows == 0) {
-             print ("<p class="error">По данному клиенту нет активного бизнес-процесса"</p>);
+             print ("<p class=\"error\">По данному клиенту нет активного бизнес-процесса</p>");
             }
             else {
                  while ($row = mysql_fetch_assoc($result)) {
@@ -19,8 +17,7 @@
                     }
                 $query2 = "SELECT `name_result_step` FROM `result_steps` WHERE `id_business_step` =$idsteps";
                 $result2 = mysql_query($query2);
-                return $step = $row[name_result_step];
-        }
+
                 while($row=mysql_fetch_assoc($result2)) {
                   $step = $row[name_result_step];
                   print ("<h3>Следубщий шаг по данному случаю - $step</h3>");
